fd2bd8a8cfe255ba48b0fca6c3a0f8587967d06f
ritka_admin <margarita_lazareva30@list.ru>
2023-06-16 10:56:29 +0300

    try to introduce noise

--- distillation.py
+++ distillation.py
@@ -6,39 +6,52 @@ from netsquid.components import (
     QSource,
     ClassicalChannel,
     SourceStatus,
+    FixedDelayModel,
+    DepolarNoiseModel,
+    FibreDelayModel,
     INSTR_SWAP,
     INSTR_MEASURE_BELL
 )
-# from netsquid.examples.purify import
+import netsquid.qubits.ketstates as ks
 from netsquid.nodes.network import Network, Node, DirectConnection
 from netsquid.qubits import StateSampler
 from src.states import *
 
 
-def create_physical_network() -> Network:
+def create_physical_network(b00_prob) -> Network:
     network = Network("Network with repeater")
 
     node_A = Node("A")
     node_B = Node("B")
     repeater = Node("Repeater")
 
-    node_A.add_subcomponent(QSource(name="QSource_A", state_sampler=StateSampler([b00]),
-                                    status=SourceStatus.EXTERNAL, num_ports=2))
-    node_B.add_subcomponent(QSource(name="QSource_B", state_sampler=StateSampler([b00]),
-                                    status=SourceStatus.EXTERNAL, num_ports=2))
+    node_A.add_subcomponent(QSource(name="QSource_A", state_sampler=StateSampler([ks.b00, ks.b01]),
+                                    probabilities=[b00_prob, 1-b00_prob],
+                                    status=SourceStatus.EXTERNAL, num_ports=2,
+                                    models={"emission_delay_model": FixedDelayModel(1e5),
+                                            "emission_noise_model": DepolarNoiseModel(time_independent=True, depolar_rate=0.5)}))
+
+    node_B.add_subcomponent(QSource(name="QSource_B", state_sampler=StateSampler([ks.b00, ks.b01]),
+                                    probabilities=[b00_prob, 1-b00_prob],
+                                    status=SourceStatus.EXTERNAL, num_ports=2,
+                                    models={"emission_noise_model": DepolarNoiseModel(time_independent=True, depolar_rate=0.5)}))
 
     # ports for qmemory communication
     node_A.subcomponents["QSource_A"].add_ports(["qout1"])
     node_B.subcomponents["QSource_B"].add_ports(["qout1"])
 
-    node_A.add_subcomponent(QuantumProcessor(name="A_memory", num_positions=2, fallback_to_nonphysical=True))
-    node_B.add_subcomponent(QuantumProcessor(name="B_memory", num_positions=2, fallback_to_nonphysical=True))
+    node_A.add_subcomponent(QuantumProcessor(name="A_memory", num_positions=2, fallback_to_nonphysical=True
+                                             , memory_noise_models=DepolarNoiseModel(1000)))
+    node_B.add_subcomponent(QuantumProcessor(name="B_memory", num_positions=2, fallback_to_nonphysical=True
+                                             , memory_noise_models=DepolarNoiseModel(1000)))
     repeater.add_subcomponent(QuantumProcessor(name="R_memory", num_positions=4, fallback_to_nonphysical=True))
 
     network.add_nodes([node_A, node_B, repeater])
 
-    channelAtoR_classical = ClassicalChannel(name="AtoR_channel_classical")
-    channelRtoA_classical = ClassicalChannel(name="RtoA_channel_classical")
+    channelAtoR_classical = ClassicalChannel(name="AtoR_channel_classical",
+                                             models={"delay_model": FibreDelayModel(c=200e3)})
+    channelRtoA_classical = ClassicalChannel(name="RtoA_channel_classical",
+                                             models={"delay_model": FibreDelayModel(c=200e3)})
 
     channelBtoR_classical = ClassicalChannel(name="BtoR_channel_classical")
     channelRtoB_classical = ClassicalChannel(name="RtoB_channel_classical")
@@ -51,8 +64,13 @@ def create_physical_network() -> Network:
     network.add_connection(node_A, repeater, connection=connectionA_R_classical)
     network.add_connection(node_B, repeater, connection=connectionB_R_classical)
 
-    channelAtoR_quantum = QuantumChannel(name="AtoR_channel_quantum")
-    channelBtoR_quantum = QuantumChannel(name="BtoR_channel_quantum")
+    channelAtoR_quantum = QuantumChannel(name="AtoR_channel_quantum",
+                                         length=20,
+                                         models={"delay_model": FibreDelayModel(c=200e3),
+                                                 "noise_model": DepolarNoiseModel(500)})
+    channelBtoR_quantum = QuantumChannel(name="BtoR_channel_quantum",
+                                         length=20,
+                                         models={"delay_model": FibreDelayModel(c=200e3)})
 
     portA, portRA = network.add_connection(node_A, repeater, channel_to=channelAtoR_quantum, label="quantum")
     portB, portRB = network.add_connection(node_B, repeater, channel_to=channelBtoR_quantum, label="quantum")
@@ -71,7 +89,7 @@ def create_physical_network() -> Network:
 
 
 if __name__ == '__main__':
-    network = create_physical_network()
+    network = create_physical_network(b00_prob=0.8)
     a = network.get_node("A")
     b = network.get_node("B")
     r = network.get_node("Repeater")
@@ -97,12 +115,20 @@ if __name__ == '__main__':
             INSTR_SWAP.execute(quantum_memory=r.qmemory, positions=[0, 2])
             INSTR_SWAP.execute(quantum_memory=r.qmemory, positions=[1, 3])
 
-    # TODO: qreprs are identical, fidelity is always high
-    fidelity_A = r.qmemory.peek([2])[0].qstate.qrepr.fidelity(r.qmemory.peek([1])[0].qstate.qrepr)
-    # res = r.qmemory.execute_instruction(INSTR_MEASURE_BELL)
+    # TODO: where to put noise model (source, processor, channel)
+    # TODO: fidelity2 is always 1.0
+    print(a.qmemory.peek([1])[0].qstate.qrepr)
+    print(r.qmemory.peek([2])[0].qstate.qrepr)
+    print(r.qmemory.peek([0, 1, 2, 3]))
+    fidelity_A = r.qmemory.peek([2])[0].qstate.qrepr.fidelity(a.qmemory.peek([1])[0].qstate.qrepr)
+    print("1: ", fidelity_A)
+
     INSTR_MEASURE_BELL.execute(quantum_memory=r.qmemory, positions=[0, 1])
     INSTR_MEASURE_BELL.execute(quantum_memory=r.qmemory, positions=[2, 3])
-    print(r.qmemory.peek([0, 1, 2, 3])[1].qstate.qrepr)
+
+    fidelity_A = r.qmemory.peek([2])[0].qstate.qrepr.fidelity(r.qmemory.peek([3])[0].qstate.qrepr)
+    print("2: ", fidelity_A)
+    # print("qrepr: ", r.qmemory.peek([2])[0].qstate.qrepr)
 
     # distill( 2 x B~R to 1 x B~R)
     #  pB ; [ (if distill B~R fails then rerun pB) until distill B~R succeeds]  ] ;
